8ec7ceb053fa14665d967c2027dcf11073f51935
Japneet <59381133+jassskalkat@users.noreply.github.com>
2023-04-22 04:50:30 -0400

    Add files via upload

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,6 +8,6 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const HomeView();
+    return HomeView();
   }
 }
