e91a4780fbc0d25aafb3d6461e5c01f7146a4cb6
Alwalid-Abushanab <aabushanab@upei.ca>
2023-04-23 12:53:31 -0300

    assignments Creation done.

--- lib/assignments/assignments_page.dart
+++ lib/assignments/assignments_page.dart
@@ -1,3 +1,4 @@
+import 'package:assign_mate/assignments/assignment_entries/new_entry/cubit/new_entry_cubit.dart';
 import 'package:assign_mate/assignments/reminder/cubit/reminder_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
